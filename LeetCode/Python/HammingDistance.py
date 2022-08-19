19d91b74561ee0a2686ffea9556607b74810aee0
leon004 <l20120965@morelia.tecnm.mx>
2022-08-19 00:12:15 -0500

    New Algorithms

---
+++ LeetCode/Python/HammingDistance.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor  = x ^ y
+        xor = "{:b}".format(xor)
+        output = 0
+        for i in xor:
+            output += int(i)
+        return output
+
+#One Line Solution
+# return sum([int(b) for b in "{:b}".format((x ^ y))])
